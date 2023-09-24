7e6054a46d1a49bea01e7cfaa7445990d682aca9
tanimoto <yumenikki2@gmail.com>
2023-09-24 21:44:12 +0900

    ルーティングテスト

---
+++ src/resources/js/Pages/inertia-test.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="テスト" />
+
+    inertiaテストです<br>
+    <a href="/">aタグ経由です。</a><br>
+    <Link href="/">LINKタグ経由です</LINK><br>
+</template>
