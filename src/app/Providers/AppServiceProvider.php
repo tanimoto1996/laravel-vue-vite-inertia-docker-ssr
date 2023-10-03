ecd09893af85225f5984c35ccc4c2b108fbab4af
tanimoto <yumenikki2@gmail.com>
2023-10-03 23:03:03 +0900

    versionメソッドを使用すると、再レンダリングが修正される（laravel-mixの場合）

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Inertia::version(function () {
+            return md5_file(public_path('mix-manifest.json'));
+        });
     }
 }
