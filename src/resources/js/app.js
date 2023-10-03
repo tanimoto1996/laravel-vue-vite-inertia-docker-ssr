b83d43b8943c5ffce846ff32c6462077b27cf4fb
tanimoto <yumenikki2@gmail.com>
2023-10-03 23:03:41 +0900

    createSSRAppへの修正

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,16 +1,23 @@
 import '../css/app.css';
-import { createApp, h } from 'vue';
+import { createSSRApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from 'ziggyVue';
+import { InertiaProgress } from "@inertiajs/progress";
+
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        const app = createApp({ render: () => h(App, props) });
+        const app = createSSRApp({ render: () => h(App, props) });
 
         app.use(ZiggyVue);
         app.use(plugin);
         app.mount(el);
     },
 });
+
+InertiaProgress.init({ color: "#000000", showSpinner: true });
--- src/resources/js/ssr.js
+++ src/resources/js/ssr.js
@@ -1,26 +1,25 @@
-import { createInertiaApp } from '@inertiajs/vue3'
-import createServer from '@inertiajs/vue3/server'
-import { renderToString } from '@vue/server-renderer'
-import { createSSRApp, h } from 'vue'
-import { ZiggyVue } from 'ziggyVue';
-import { Ziggy } from './ziggy';
+import { createSSRApp, h } from 'vue';
+import { renderToString } from '@vue/server-renderer';
+import { createInertiaApp } from '@inertiajs/vue3';
+import createServer from '@inertiajs/vue3/server';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-createServer(page =>
-	createInertiaApp({
-		page,
-		render: renderToString,
-		resolve: name => {
-			const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-			return pages[`./Pages/${name}.vue`]
-		},
-		setup({ App, props, plugin }) {
-			const ssr = createSSRApp({ render: () => h(App, props) });
-
-			ssr.use(ZiggyVue, Ziggy);
-			ssr.use(plugin);
-
-			return ssr;
-		},
-	}),
-)
+createServer((page) =>
+    createInertiaApp({
+        page,
+        render: renderToString,
+        title: (title) => `${title} - ${appName}`,
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+        setup({ App, props, plugin }) {
+            return createSSRApp({ render: () => h(App, props) })
+                .use(plugin)
+                .use(ZiggyVue, {
+                    ...page.props.ziggy,
+                    location: new URL(page.props.ziggy.location),
+                });
+        },
+    })
+);
